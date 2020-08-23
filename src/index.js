ea74c991e989d3664928d1409538250dfa835906
nach131 <nacho.mota@gmail.com>
2020-08-23 11:56:22 +0200

    INICIO

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from './components/Header';
+// import Intro from './components/Intro';
+import Gallery from './components/Gallery';
+
+import "./styles.css";
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0e1525'
+    },
+    secondary: {
+      main: '#c93838'
+    },
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        {/* <Intro /> */}
+        <Gallery />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
